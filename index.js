3ceca2e26950b9303e290fd291798727fdf8cc86
aLehav <88849702+aLehav@users.noreply.github.com>
2021-11-12 22:52:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var app = express();
+const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000
 
@@ -9,3 +9,7 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.get('/',function(req, res) {
+  res.sendFile(path.join(__dirname + '/views/pages/scopeCup.html')
+});
