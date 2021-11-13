2540e07c59e48bea2b98a4ef421df7ff0d03c4d4
aLehav <88849702+aLehav@users.noreply.github.com>
2021-11-12 22:45:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static("public"));
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
